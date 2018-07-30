76c5d9a006f0fbe633cb72eb5ca15ef0c89ac159
FreddyJ <freddyjchisale@gmail.com>
2018-07-30 03:54:20 +0200

    Web API application

---
+++ CustomerContactManager/CustomerContactManager/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CustomerContactManager.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
---
+++ CustomerContactManager/CustomerContactManager/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CustomerContactManager.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
